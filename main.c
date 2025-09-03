abd68116f4619b2291de8a9a6dddf5a0ffb423e2
Danyael-code <danyaelarjantolawin@gmail.com>
2025-09-03 20:01:59 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //Var Def
+    int length, width, area;
+    
+    //Var Init
+    printf("Calculate Area of Rectangle");
+    length = 5;
+    width = 3;
+    area = length*width;
+    printf("Area of Rectangle = %d \n", area);
+    
+    return 0;
+}
